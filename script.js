685af55ac4c32323acd99aa2cf7d6cfbb54d4bf9
chrisrandledev <117551889+chrisrandledev@users.noreply.github.com>
2023-01-24 23:22:38 +0000

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+function scrollAction(action) {
+  if (action === 1) {
+    console.log('Scrolling parent to TOP!');
+  } else if (action === 2) {
+  } else if (action === 3) {
+  } else if (action === 4) {
+  } else {
+    throw new Error('Unable to handle action.');
+  }
+}
