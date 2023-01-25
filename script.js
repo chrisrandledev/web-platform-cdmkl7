42ffc30ad21b8c5c67e16321977d1222efb8b115
chrisrandledev <117551889+chrisrandledev@users.noreply.github.com>
2023-01-25 03:41:29 +0000

    1 file modified

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function test() {
 
 function scrollAction(action) {
   console.log('Action: ' + action);
+  var iframe = document.getElementById('iframe1');
   switch (action) {
     case 1:
       window.parent.scrollTo(0, 0);
@@ -12,12 +13,10 @@ function scrollAction(action) {
       window.parent.scrollTo(0, window.parent.document.body.scrollHeight);
       break;
     case 3:
-      document.getElementById('iframe1').contentWindow.scrollTo(0, 0);
+      if (iframe) iframe.contentWindow.scrollTo(0, 0);
       break;
     case 4:
-      document
-        .getElementById('iframe1')
-        .contentWindow.scrollTo(0, document.body.scrollHeight);
+      if (iframe) iframe.contentWindow.scrollTo(0, iframe.scrollHeight);
       break;
     default:
       console.error('An error occurred.');
